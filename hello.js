726315204a2ffd314b879b4600fda517bf18fa78
Tejpatel1806 <tejpatel1806@gmail.com>
2024-01-09 23:24:29 +0530

    Update

---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("Hii !! I am a Tej Patel");
+// console.log(window); gives a error
+const math=require("./math");
+console.log(math);
+//{ addfun: [Function: add], subfun: [Function: sub] }
+
+console.log(math.addfun(2,3));
