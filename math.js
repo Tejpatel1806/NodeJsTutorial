1fa243d7d3d304ddc48533d0d3738febef158aa8
tejpatel <tejpatel1806@gmail.com>
2024-01-10 18:56:55 +0530

    filesystem

--- math.js
+++ math.js
@@ -6,4 +6,4 @@ function sub(a,b)
 {
     return a-b;
 }
-module.exports={addfun:add,subfun:sub};
+module.exports={addfun:add,subfun:sub};//same as module.exports={add,sub}; ane biji baju hello.js ma aapde add ane sub kari ne use kari saksu like const {add,sub}=require("./math.js");
